bdc562699995f1ff53c9c8a47358c5a6e1a5e4a3
niqy1988 <niqy1988@gmail.com>
2020-05-25 04:11:58 +0100

    gamedatabundle package

---
+++ gamedatabundle.py
@@ -0,0 +1,68 @@
+import os
+import copy
+import json
+import yaml
+import uuid
+
+from typing import Optional, List
+
+# load general config
+with open('config/general.yaml') as general_config_file:
+    general_config = yaml.safe_load(general_config_file)
+    uuid_namespace_prefix = general_config['uuid_namespace_prefix']
+
+
+# uuid generating function
+def get_uuid(debug_name: str) -> str:
+    return str(uuid.uuid5(uuid.NAMESPACE_DNS, '{0}.{1}'.format(
+        uuid_namespace_prefix, debug_name
+    )))
+
+
+class GameDataBundle:
+    gamedatabundle_dict = None
+    gamedataobject_template = None
+    conditional_component_template = None
+
+    def __init__(self):
+        # load template
+        with open('template/gamedatabundle.json') as gamedatabundle_template_file:
+            self.gamedatabundle_dict = json.load(gamedatabundle_template_file)
+        with open('template/gamedataobject_ins_aff.json') as gamedataobject_template_file:
+            self.gamedataobject_template = json.load(gamedataobject_template_file)
+        with open('template/conditional_component.json') as conditional_component_template_file:
+            self.conditional_component_template = json.load(conditional_component_template_file)
+
+    def insert_ins_aff(self,
+                       debug_name: str,
+                       display_name_index: int,
+                       subject_uuid: str,
+                       status_uuids: List[str],
+                       category_uuid: str):
+
+        # create new gamedataobject
+        gamedataobject = copy.deepcopy(self.gamedataobject_template)
+        gamedataobject['DebugName'] = debug_name
+        gamedataobject['ID'] = get_uuid(debug_name)
+        gamedataobject['Components'][0]['DisplayName'] = display_name_index
+        gamedataobject['Components'][0]['AIConditionalCategoryID'] = category_uuid
+
+        # insert identifier_uuid checks
+        for status_uuid in status_uuids:
+            conditional_component = copy.deepcopy(self.conditional_component_template)
+            conditional_component['Data']['Parameters'] = [
+                subject_uuid, status_uuid
+            ]
+            gamedataobject['Components'][0]['ConditionalScripts']['Components'].append(
+                conditional_component
+            )
+
+        # add gamedataobject to gamedatabundle
+        self.gamedatabundle_dict['GameDataObjects'].append(gamedataobject)
+
+    def write(self, file_path: str):
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, 'w') as f:
+            json.dump(self.gamedatabundle_dict, f)
+
+
--- generate_mod.py
+++ generate_mod.py
@@ -2,38 +2,38 @@ import uuid
 import xml
 import json
 import yaml
+
 from stringtable import StringTable
+from gamedatabundle import GameDataBundle
+
 
 # load all config files
-configs = {}
+config = {}
 with open('config/general.yaml') as general_config_file:
-    configs['general'] = yaml.safe_load(general_config_file)
+    config['general'] = yaml.safe_load(general_config_file)
 with open('config/inspiration.yaml') as inspiration_config_file:
-    configs['inspiration'] = yaml.load(inspiration_config_file, Loader=yaml.BaseLoader)
+    config['inspiration'] = yaml.load(inspiration_config_file, Loader=yaml.BaseLoader)
 with open('config/affliction.yaml') as affliction_config_file:
-    configs['affliction'] = yaml.load(affliction_config_file, Loader=yaml.BaseLoader)
+    config['affliction'] = yaml.load(affliction_config_file, Loader=yaml.BaseLoader)
 
 
-# uuid generating function
-def get_uuid(debug_name: str) -> str:
-    return str(uuid.uuid5(uuid.NAMESPACE_DNS, '{0}.{1}'.format(
-        configs['general']['uuid_namespace_prefix'], debug_name
-    )))
+# generate configuration for self inspirations
+condition_subject = 'self'
+condition_type = 'inspiration'
+category_name = f'{condition_subject}_{condition_type}'
+subject_uuid = config['general']['system_uuid']['condition_subject'][condition_subject]
+category_uuid = config['general']['system_uuid']['condition_category'][category_name]
 
+game_data_bundle = GameDataBundle()
+game_data_bundle.insert_ins_aff('test_cond', 123, subject_uuid, [subject_uuid, category_uuid], category_uuid)
+game_data_bundle.insert_ins_aff('test_cond2', 321, category_uuid, [category_uuid, subject_uuid], subject_uuid)
+game_data_bundle.write('output/gamedata/bundle.json')
 
-# generating configuration for self inspirations
-
-print('{0}.{1}'.format(
-    configs['general']['uuid_namespace_prefix'], 'abb'
-))
-print(get_uuid('abv'))
-print(get_uuid('abb'))
-print(get_uuid('abb'))
-print(get_uuid('abv'))
+print(subject_uuid)
+print(category_uuid)
 
 a = StringTable('game\customai', 17301)
-print(a.insert_entry('some text'))
-print(a.insert_entry('some other texts'))
-print(a.insert_entry('a third entry'))
-
-a.write('output/output.xml')
+print(a.insert('some text'))
+print(a.insert('some other texts'))
+print(a.insert('a third entry'))
+# a.write('output/dir/another/output.xml')
