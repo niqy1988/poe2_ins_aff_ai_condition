466ab939d1c7e658ec861062c88bbaff3c003923
niqy1988 <niqy1988@gmail.com>
2020-05-25 02:27:29 +0100

    add stringtable class

--- generate_mod.py
+++ generate_mod.py
@@ -1,4 +1,5 @@
 import uuid
+from stringtable import StringTable
 
 NAMESPACE_PREFIX = 'mod.poe2.ins_aff_ai_condition.'
 
@@ -6,3 +7,10 @@ def get_uuid(debug_name: str) -> str:
     return str(uuid.uuid5(uuid.NAMESPACE_DNS, NAMESPACE_PREFIX + debug_name))
 
 print(get_uuid('abv'))
+
+a = StringTable('game\customai', 17301)
+print(a.insert_entry('some text'))
+print(a.insert_entry('some other texts'))
+print(a.insert_entry('a third entry'))
+
+a.write('output/output.xml')
---
+++ stringtable.py
@@ -0,0 +1,48 @@
+import xml.etree.ElementTree as ET
+
+from typing import Optional
+
+
+class StringTable:
+    name = ''
+    start_index = None
+    next_index = None
+    nametable_tree = None
+    entry_node = None
+
+
+    def __init__(self, name: str, start_index: int):
+        # load template
+        with open('template/stringtable.xml') as stringtable_template_file:
+            self.nametable_tree = ET.parse(stringtable_template_file)
+
+        # initialize class variables
+        self.name = name
+        self.start_index = start_index
+        self.next_index = start_index
+
+        # initialize nametable tree
+        tree_root = self.nametable_tree.getroot()
+        tree_root.find('Name').text = self.name
+        self.entry_node = tree_root.find('Entries')
+
+
+    def insert_entry(self, entry_text: str, entry_index: Optional[int] = None) -> int:
+        if entry_index is None:
+            entry_index = self.next_index
+
+        # load template
+        with open('template/stringtable_entry.xml') as entry_template_file:
+            new_entry = ET.parse(entry_template_file).getroot()
+
+        # insert new entry
+        new_entry.find('ID').text = str(entry_index)
+        new_entry.find('DefaultText').text = entry_text
+        self.entry_node.append(new_entry)
+        self.next_index += 1
+
+        return entry_index
+
+
+    def write(self, file_path: str):
+        self.nametable_tree.write(file_path, encoding='utf-8', xml_declaration=True)
