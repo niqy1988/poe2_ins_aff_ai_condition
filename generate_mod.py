aeb86fc38d13c2d9bfb0498523ea8084ec3013b3
niqy1988 <niqy1988@gmail.com>
2020-05-25 01:22:36 +0100

    initial test to load configs

--- generate_mod.py
+++ generate_mod.py
@@ -1,8 +1,28 @@
 import uuid
+import xml
+import json
+import yaml
 
-NAMESPACE_PREFIX = 'mod.poe2.ins_aff_ai_condition.'
+# load all config files
+configs = {}
+with open('configs/general.yaml') as general_config_file:
+    configs['general'] = yaml.safe_load(general_config_file)
+with open('configs/inspirations.yaml') as inspirations_config_file:
+    configs['inspirations'] = yaml.load(inspirations_config_file, Loader=yaml.BaseLoader)
+with open('configs/afflictions.yaml') as afflictions_config_file:
+    configs['afflictions'] = yaml.load(afflictions_config_file, Loader=yaml.BaseLoader)
 
+# uuid generating function
 def get_uuid(debug_name: str) -> str:
-    return str(uuid.uuid5(uuid.NAMESPACE_DNS, NAMESPACE_PREFIX + debug_name))
+    return str(uuid.uuid5(uuid.NAMESPACE_DNS, '{0}.{1}'.format(
+        configs['general']['uuid_namespace_prefix'], debug_name
+    )))
 
+# generating configuration for self inspirations
+
+print('{0}.{1}'.format(
+        configs['general']['uuid_namespace_prefix'], 'abb'
+    ))
 print(get_uuid('abv'))
+print(get_uuid('abb'))
+print(get_uuid('abb'))
