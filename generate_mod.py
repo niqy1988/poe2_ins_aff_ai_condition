8f1a45b82c7cba7159ce86d53833338570f17da1
niqy1988 <niqy1988@gmail.com>
2020-05-25 01:31:15 +0100

    reformat code

--- generate_mod.py
+++ generate_mod.py
@@ -12,17 +12,19 @@ with open('configs/inspiration.yaml') as inspiration_config_file:
 with open('configs/affliction.yaml') as affliction_config_file:
     configs['affliction'] = yaml.load(affliction_config_file, Loader=yaml.BaseLoader)
 
+
 # uuid generating function
 def get_uuid(debug_name: str) -> str:
     return str(uuid.uuid5(uuid.NAMESPACE_DNS, '{0}.{1}'.format(
         configs['general']['uuid_namespace_prefix'], debug_name
     )))
 
+
 # generating configuration for self inspirations
 
 print('{0}.{1}'.format(
-        configs['general']['uuid_namespace_prefix'], 'abb'
-    ))
+    configs['general']['uuid_namespace_prefix'], 'abb'
+))
 print(get_uuid('abv'))
 print(get_uuid('abb'))
 print(get_uuid('abb'))
